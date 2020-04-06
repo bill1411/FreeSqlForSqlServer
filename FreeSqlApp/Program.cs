2c57129edd749aa850b393169f45bb0ca41f2a9c
木易 <oncwnuKMS2M73vNWCP6rN8E61Lh0@git.weixin.qq.com>
2020-04-06 22:33:21 +0800

    新增查询模块

---
+++ FreeSqlApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using FindToilet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeSqlApp
+{
+    class Program
+    {
+        static readonly string connstr = "Data Source=.;Integrated Security=True;Initial Catalog=FindToilet;Pooling=true;Min Pool Size=1";
+
+        static IFreeSql fsql = new FreeSql.FreeSqlBuilder()
+            .UseConnectionString(FreeSql.DataType.SqlServer, connstr)
+            .UseAutoSyncStructure(true) //自动同步实体结构到数据库
+            .Build(); //请务必定义成 Singleton 单例模式
+
+        static void Main(string[] args)
+        {
+            Select();
+        }
+
+        #region 查询
+        private static void Select()
+        {
+            List<T_City> list =  fsql.Queryable<T_City>().Where(x=>x.name.Contains("省")).ToList();
+            foreach (var item in list)
+            {
+                Console.WriteLine("所属省份：" + item.name);
+            }
+            Console.ReadKey();
+        }
+        #endregion
+    }
+}
